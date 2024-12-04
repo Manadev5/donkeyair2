a7f50ecaba9f65cfcb05711bda27a87d53b5ab3b
Manassè MUKENGE <manakabamba@gmail.com>
2024-12-04 16:46:47 +0100

    First connection to api

--- front/src/App.js
+++ front/src/App.js
@@ -2,21 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const fetchDatas = async () => {
+    try {
+          const response = await fetch('https://localhost:7014/api/Admins', {
+              method: 'GET',
+              headers: {
+                  'Content-Type': 'application/json',
+              },
+          });
+          if (!response.ok) {
+              throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          const data = await response.json();
+          console.log(data);
+      } catch (error) {
+          console.error('Erreur lors de la récupération des données:', error);
+      }
+    }
+
+    fetchDatas();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Manasse est beau !</p>
       </header>
     </div>
   );
